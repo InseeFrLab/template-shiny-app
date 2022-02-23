d72859db3578ab14118e26bca56e592044941a77
etgtk6 <romain.avouac@insee.fr>
2022-02-23 10:53:33 +0000

    fix null values

--- myshinyapp/inst/app/server.R
+++ myshinyapp/inst/app/server.R
@@ -4,37 +4,39 @@ server <- function(input, output) {
   data <- reactive({
     # Connect to the DB
     conn <- DBI::dbConnect(
-    RPostgres::Postgres(),
-    dbname = Sys.getenv("POSTGRESQL_DB_NAME"),
-    host = Sys.getenv("POSTGRESQL_DB_HOST"),
-    port = Sys.getenv("POSTGRESQL_DB_PORT"),
-    user = Sys.getenv("POSTGRESQL_DB_USER"),
-    password = Sys.getenv("POSTGRESQL_DB_PASSWORD")
+      RPostgres::Postgres(),
+      dbname = Sys.getenv("POSTGRESQL_DB_NAME"),
+      host = Sys.getenv("POSTGRESQL_DB_HOST"),
+      port = Sys.getenv("POSTGRESQL_DB_PORT"),
+      user = Sys.getenv("POSTGRESQL_DB_USER"),
+      password = Sys.getenv("POSTGRESQL_DB_PASSWORD")
     )
-
+    
     # Get the data
     quakes <- DBI::dbGetQuery(conn, glue::glue("SELECT * FROM quakes WHERE mag >= {input$magSlider}"))
+    quakes <- data.frame(quakes)
     
     # Disconnect from the DB
     DBI::dbDisconnect(conn)
     
-    # Convert to data.frame
-    data.frame(quakes)
+    return(quakes)
   })
-
-  # Render map
+  
+  # Base map
   output$map <- leaflet::renderLeaflet({
-    mymap <- leaflet::leaflet(data = data())
+    mymap <- leaflet::leaflet()
     mymap <- leaflet::addTiles(mymap)
-    mymap <- leaflet::addMarkers(mymap, ~long, ~lat, label = ~mag)
+    mymap <- leaflet::setView(mymap, lng=178,lat=-23,zoom=4)
     mymap <- leaflet::addProviderTiles(mymap, leaflet::providers$Esri.WorldStreetMap)
   })
-
+  
   # Update markers
   observe({
     proxy <- leaflet::leafletProxy("map")
     proxy <- leaflet::clearMarkers(proxy)
-    proxy <- leaflet::addMarkers(proxy, ~long, ~lat, label = ~mag)
-  })
-
+    if (nrow(data()) > 0) {
+    proxy <- leaflet::addMarkers(proxy, data=data(), ~long, ~lat, label = ~mag)  
+    }
+    })
+  
 }
