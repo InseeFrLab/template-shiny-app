c088884349673cbb1474b65ac33007dd8d5e5b30
Romain Avouac <43444134+avouacr@users.noreply.github.com>
2022-04-04 17:46:51 +0200

    fix csv location

--- myshinyapp/R/main.R
+++ myshinyapp/R/main.R
@@ -1,6 +1,6 @@
 
 runApp <- function(){
-  myshinyapp::populate_table(bucket = "etgtk6", object = "diffusion/shiny-template/quakes.csv")
+  myshinyapp::populate_table(bucket = "avouacr", object = "diffusion/shiny-template/quakes.csv")
   appDir <- system.file("app", package = "myshinyapp")
   shiny::runApp(appDir, display.mode = "normal")
 }
