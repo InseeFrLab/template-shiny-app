bdd038310d131e486af9072a269192309719d264
Romain Avouac <43444134+avouacr@users.noreply.github.com>
2022-04-04 17:07:24 +0200

    externalize minio populate

--- myshinyapp/R/main.R
+++ myshinyapp/R/main.R
@@ -1,11 +1,6 @@
 
-load_data <- function() {
-  myshinyapp::to_minio(bucket = "etgtk6", object = "diffusion/shiny-template/quakes.csv")
-  myshinyapp::populate_table(bucket = "etgtk6", object = "diffusion/shiny-template/quakes.csv")
-}
-
 runApp <- function(){
-  myshinyapp::load_data()
+  myshinyapp::populate_table(bucket = "etgtk6", object = "diffusion/shiny-template/quakes.csv")
   appDir <- system.file("app", package = "myshinyapp")
   shiny::runApp(appDir, display.mode = "normal")
 }
